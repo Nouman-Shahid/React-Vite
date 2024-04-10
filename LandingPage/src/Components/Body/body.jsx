f4a19e72ebb9ed9d80f14008ff431fb58011f505
Numan Shahid <noumanshahid2486@gmail.com>
2024-04-10 18:09:15 +0500

    code refactoring

--- LandingPage/src/Components/Body/body.jsx
+++ LandingPage/src/Components/Body/body.jsx
@@ -2,20 +2,10 @@ import React from 'react'
 
 const body = () => {
     return (
-        <main className=' bg-gradient-to-r from-[#0D0E10] to-[#303538] flex px-24 justify-between items-center  h-screen'>
+        <main className='bg-gradient-to-r from-[#0D0E10] to-[#303538] h-[195vh]' >
 
-            <div className="flex-col  ">
-                <p className='text-4xl font-sans font-bold my-3'>Build your Apps with Bertix</p>
-                <p className='text-2xl w-96 font-sans  my-3'>We are team of talented designers & developers</p>
-                <button className='hover:bg-blue-400  text-1xl py-1 px-2 font-sans rounded-3xl bg-blue-300 my-3'>Get Started</button>
+            <img src="public/burger.jpg" className='mix-blend-screen h-auto' />
 
-            </div>
-
-
-            <div className="flex items-center  ">
-
-                <img className='mix-blend-darken h-80 rounded-3xl' src="public/mobile.png" alt="" />
-            </div>
 
 
         </main>
--- LandingPage/src/Components/Navbar/navbar.jsx
+++ LandingPage/src/Components/Navbar/navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                 <h1 className='text-white w-fit font-sans font-bold text-center text-2xl sm:text-3xl'>Feane</h1>
 
 
-                <ul className={`flex-col p-3 fixed top-[7vh] right-0 space-y-3 bg-transparent md:flex-row md:static md:flex  md:space-x-6 md:text-2xl   ${isClicked ? "flex" : "hidden"}`}>
+                <ul className={` flex-col p-3 absolute top-[7vh] right-0 space-y-3 bg-transparent md:flex-row md:static md:flex  md:space-x-6 md:text-2xl   ${isClicked ? "flex" : "hidden"}`}>
                     <li ></li>
                     <li className='text-gray-500 cursor-pointer hover:text-red-500 focus:border-2 focus:border-red-500'>Home</li>
                     <li className='text-gray-500 cursor-pointer hover:text-red-500 focus:border-2 focus:border-red-500'>Menu</li>
