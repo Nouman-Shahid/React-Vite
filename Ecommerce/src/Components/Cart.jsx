f931eda15df12132b189f4150705f656dfca372e
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-04-27 13:22:41 +0500

    code refactoring

--- Ecommerce/src/Components/Cart.jsx
+++ Ecommerce/src/Components/Cart.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 const Cart = () => {
-    return (
-        <>
+  return (
+    <>
+      <h1 to="/cart" className="text-center py-16">
+        Cart
+      </h1>
+    </>
+  );
+};
 
-            <h1 to='/cart' className='text-center py-16'>Cart</h1>
-
-        </>
-    )
-}
-
-export default Cart
+export default Cart;
